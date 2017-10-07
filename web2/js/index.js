10c2965ba7f07c660cd6a0cbfabdeb28449d3b87
892499846@qq.com <892499846@qq.com>
2017-10-07 11:49:21 +0800

    add

---
+++ web2/js/index.js
@@ -0,0 +1,37 @@
+var nav = [
+	{
+		name : "法制档案",
+		items: ["法律","行政法规","司法解释"],
+		url : "www.baidu.com"
+	},
+	{
+		name : "法制文化",
+		items: ["法制漫画","法制文化作品","普法宣传资料"],
+		url : "www.baidu.com"
+	},
+	{
+		name : "法制动态",
+		items: ["普法","新法速递"],
+		url : "www.baidu.com"
+	},
+	{
+		name : "法制服务",
+		items: ["法律服务","办事指南","法律援助","公证服务","人民调节"],
+		url : "www.baidu.com"
+	},
+	{
+		name : "法制档案",
+		items: ["法律","行政法规","司法解释"],
+		url : "www.baidu.com"
+	},
+	{
+		name : "法制档案",
+		items: ["法律","行政法规","司法解释"],
+		url : "www.baidu.com"
+	},
+	{
+		name : "法制档案",
+		items: ["法律","行政法规","司法解释"],
+		url : "www.baidu.com"
+	},
+]
